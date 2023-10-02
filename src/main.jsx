e3b63eb4833aa2b15dbf92931e10124b77b8b04a
caue-bueno <caue-bueno@live.com>
2023-10-02 19:07:48 -0300

    Created theme.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { SignIn } from './pages/SignIn/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>,
 )

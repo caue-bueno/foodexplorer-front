f964ecc878c9925e750390fd9a32af090a91b013
caue-bueno <caue-bueno@live.com>
2023-10-08 11:54:10 -0300

    Created global.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global'
+
 import { SignIn } from './pages/SignIn/index.jsx'
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SignIn />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <SignIn />
+    </ThemeProvider>
   </React.StrictMode>,
 )
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,35 +1,26 @@
-export default {
-  COLORS: {
+import { createGlobalStyle } from "styled-components";
 
-    LIGHT_100: '#FFF',
-    LIGHT_200: '#FFFAF1',
-    LIGHT_300: '#E1E1E6',
-    LIGHT_400: '#C4C4CC',
-    LIGHT_500: '#7C7C8A',
-    LIGHT_600: '#76797B',
-    LIGHT_700: '#4D585E',
+export default createGlobalStyle`
+  * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
-    DARK_100: '#000405',
-    DARK_200: '#00070A',
-    DARK_300: '#000204',
-    DARK_400: '#000A0F',
-    DARK_500: '#000C12',
-    DARK_600: '#00111A',
-    DARK_700: '#001119',
-    DARK_800: '#0D161B',
-    DARK_900: '#0D1D25',
-    DARK_1000: '#192227',
+    body {
+        background-color: ${({ theme }) => theme.COLORS.DARK_400};
+        color: ${({ theme }) => theme.COLORS.LIGHT_100};
 
-    TOMATO_100: '#750310',
-    TOMATO_200: '#92000E',
-    TOMATO_300: '#AB222E',
-    TOMATO_400: '#AB4D55',
-    
-    CARROT_100: '#FBA94C',
+        -webkit-font-smoothing: antialiased;
+    }
 
-    MINT_100: '#04D361',
+    a {
+        text-decoration: none;
+    }
 
-    CAKE_100: '#065E7C',
-    CAKE_200: '#82F3FF',
-  }
-}
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+`;
